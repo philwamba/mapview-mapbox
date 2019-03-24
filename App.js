fba156e37d1ea882fdc204ad494719f870483d75
Phil Wamba <philwamba@gmail.com>
2019-03-24 09:55:40 +0300

    Installed Mapbox and added mapview

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import MapboxGL from "@mapbox/react-native-mapbox-gl";
+
+MapboxGL.setAccessToken("YOUR API KEY HERE");
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <MapboxGL.MapView
+          styleURL={MapboxGL.styleURL}
+          zoomLevel={16}
+          userTrackingMode={1}
+          showUserLocation={true}
+          style={{
+            flex: 1
+          }}
+        />
       </View>
     );
   }
@@ -14,13 +25,6 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     backgroundColor: "#ffffff"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
   }
 });
